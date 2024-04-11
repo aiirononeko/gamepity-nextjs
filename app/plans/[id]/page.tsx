493e605952e9ae38870ac830437049d9c4aee091
aiirononeko <aiirononeko@gmail.com>
2024-04-11 16:06:23 +0900

    日時選択UIのモック作成

--- app/plans/[id]/page.tsx
+++ app/plans/[id]/page.tsx
@@ -1,15 +1,30 @@
 import { getAvailableDateTimes } from '@/data/availableDateTime'
 import { getPlan } from '@/data/plan'
 import PlanCard from '@/app/streamers/[id]/components/PlanCard'
+import AvailableDateTimeTable from './components/AvailableDateTimeTable'
 
 export default async function Page({ params }: { params: { id: string } }) {
   const plan = await getPlan(Number(params.id))
-  const availableDateTime = await getAvailableDateTimes(plan.streamer_id)
+  const availableDateTimes = await getAvailableDateTimes(plan.streamer_id)
 
   return (
     <div className='container mx-auto mt-10'>
-      <p className='mb-4 text-xl font-bold text-game-white'>選択中のプラン</p>
-      <PlanCard plan={plan} />
+      <div className='mb-10'>
+        <p className='mb-4 text-xl font-bold text-game-white'>選択中のプラン</p>
+        <PlanCard plan={plan} />
+      </div>
+      <div className='mb-10'>
+      {availableDateTimes && availableDateTimes.length > 0 ? (
+        <AvailableDateTimeTable availableDateTimes={availableDateTimes} />
+      ) : (
+        <p className='text-center text-xl font-bold text-game-white'>
+          予約可能な日時がありません
+        </p>
+      )}
+      </div>
+      <div className='mb-10'>
+        <p className='mb-4 text-xl font-bold text-game-white text-center'>※予約時の注意事項</p>
+      </div>
     </div>
   )
 }
--- data/availableDateTime.ts
+++ data/availableDateTime.ts
@@ -13,7 +13,6 @@ export async function getAvailableDateTimes(
     .from('available_date_times')
     .select('*')
     .eq('streamer_id', streamerId)
-    .limit(1)
 
   if (error) throw new Error(error.message)
 
