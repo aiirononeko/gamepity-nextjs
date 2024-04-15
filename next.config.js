693574d321eb42729b3206750fbf2045e7403c1f
aiirononeko <aiirononeko@gmail.com>
2024-04-15 13:34:19 +0900

    hosts設定

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dgcqfzhophkhekhfppvl.supabase.co',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
