ecdaf547dd365427d6f476deddaaf948873af4c2
aiirononeko <aiirononeko@gmail.com>
2024-05-06 15:00:22 +0900

    グラデーションボタンを共通コンポーネントとして切り出し

---
+++ components/GradationButton.tsx
@@ -0,0 +1,11 @@
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: React.ReactNode
+}
+
+export function GradationButton({ children }: Props) {
+  return (
+    <button className='rounded border-2 border-solid border-game-white bg-gradient-to-r from-[#FFB13C] to-[#EF3CFF] px-8 py-3 text-game-white'>
+      {children}
+    </button>
+  )
+}
