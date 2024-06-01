86a2c3cf7676719141139c82a7f6e75e28d3ce04
aiirononeko <aiirononeko@gmail.com>
2024-06-01 23:56:30 +0900

    ストリーマーページにも各種ボタン配置

---
+++ app/streamers/mypage/components/SignOutForm.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { signOut } from '@/actions/auth'
+import { Button } from '@/components/ui/button'
+import { Form } from '@/components/ui/form'
+import { Loader2 } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+
+export default function SignOutForm() {
+  const form = useForm()
+  const onSubmit = async () => {
+    await signOut()
+    toast.success('ログアウトしました', {
+      position: 'top-right',
+      duration: 3000,
+    })
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='flex flex-col items-center space-y-8 md:items-start'
+      >
+        <Button
+          disabled={!form.formState.isValid || form.formState.isSubmitting}
+          className='w-40'
+          variant='outline'
+        >
+          {form.formState.isSubmitting && (
+            <Loader2 className='mr-2 size-4 animate-spin' />
+          )}
+          ログアウト
+        </Button>
+      </form>
+    </Form>
+  )
+}
---
+++ app/streamers/mypage/components/WithdrawalForm.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { withdrawal } from '@/actions/auth'
+import { Button } from '@/components/ui/button'
+import { Form } from '@/components/ui/form'
+import { Loader2 } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+
+type Props = {
+  userId: string
+}
+
+export default function WithdrawalForm({ userId }: Props) {
+  const form = useForm()
+  const onSubmit = async () => {
+    await withdrawal(userId)
+    toast.success('退会しました', {
+      position: 'top-right',
+      duration: 3000,
+    })
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='flex flex-col items-center space-y-8 md:items-start'
+      >
+        <Button
+          disabled={!form.formState.isValid || form.formState.isSubmitting}
+          className='w-40'
+          variant='destructive'
+        >
+          {form.formState.isSubmitting && (
+            <Loader2 className='mr-2 size-4 animate-spin' />
+          )}
+          退会
+        </Button>
+      </form>
+    </Form>
+  )
+}
